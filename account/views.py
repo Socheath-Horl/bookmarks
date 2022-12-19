715b27bc1b422d4a861ce32e19673da8d0f0e6a7
Horl-Socheath <horl.socheath@gmail.com>
2022-12-19 20:43:11 +0700

    create login view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,33 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+from .forms import LoginForm
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request=request,
+                username=cd['username'],
+                password=cd['password']
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request=request, user=user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(
+        request=request,
+        template_name='account/login.xhtml',
+        context={
+            'form': form
+        }
+    )
