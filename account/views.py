18907039222092575191ddabd39fe139ac9bfa9c
Horl-Socheath <horl.socheath@gmail.com>
2023-01-09 21:41:49 +0700

    add register user

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 def user_login(request):
     if request.method == 'POST':
@@ -42,3 +42,27 @@ def dashboard(request):
             'section': 'dashboard'
         }
     )
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(
+                request=request,
+                template_name='account/register_done.html',
+                context={'new_user': new_user}
+            )
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request=request,
+        template_name='account/register.html',
+        context={'user_form': user_form}
+    )
+    
